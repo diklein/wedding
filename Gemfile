b6061b8fa6c04efb9641317db19652ea46348da1
David Klein <me@diklein.com>
2017-09-09 11:08:10 -0700

    FIrst commit

---
+++ Gemfile
@@ -0,0 +1,14 @@
+source "https://rubygems.org"
+
+gem "jekyll"
+
+group :jekyll_plugins do
+  gem "jekyll-assets"
+end
+
+gem "coffee-script"
+gem "facets"
+gem "react-jsx-sprockets"
+gem "redcarpet"
+gem "sprockets-es6"
+gem "uglifier"
--- gulpfile.js
+++ gulpfile.js
@@ -1,60 +1,114 @@
 // Gulp tasks for JKL Tachyons
 
-// Load plugins
-var gulp = require('gulp'),
-    gutil = require('gulp-util'),
-    basswork = require('gulp-basswork'),
-    watch = require('gulp-watch'),
-    prefix = require('gulp-autoprefixer'),
-    //uncss = require('gulp-uncss'),
-    minifyCSS = require('gulp-minify-css'),
-    //sass = require('gulp-sass'),
-    size = require('gulp-size'),
-    rename = require('gulp-rename'),
-    csslint = require('gulp-csslint'),
-    css = require('css'),
-    browserSync = require('browser-sync'),
-    browserReload = browserSync.reload;
+// Default plugins
+// var gulp = require('gulp'),
+//     gutil = require('gulp-util'),
+//     basswork = require('gulp-basswork'),
+//     watch = require('gulp-watch'),
+//     prefix = require('gulp-autoprefixer'),
+//     //uncss = require('gulp-uncss'),
+//     minifyCSS = require('gulp-minify-css'),
+//     //sass = require('gulp-sass'),
+//     size = require('gulp-size'),
+//     rename = require('gulp-rename'),
+//     csslint = require('gulp-csslint'),
+//     css = require('css'),
+//     browserSync = require('browser-sync'),
+//     browserReload = browserSync.reload;
 
-gulp.task('css', function() {
-  gulp.src('./src/jkl-tachyons.css')
-    .pipe(basswork())
-    .pipe(size({gzip: false, showFiles: true, title:'basswork css'}))
-    .pipe(size({gzip: true, showFiles: true, title:'basswork gzipped css'}))
-    .pipe(gulp.dest('./css'))
-    .pipe(minifyCSS())
-    .pipe(rename({ extname: '.min.css' }))
-    .pipe(size({gzip: false, showFiles: true, title:'basswork minified'}))
-    .pipe(size({gzip: true, showFiles: true, title:'basswork minified'}))
-    .pipe(gulp.dest('./css'));
+// DE plugins
+var gulp         = require('gulp');
+var util         = require('util');
+var postcss      = require('gulp-postcss');
+var cssnext      = require('postcss-cssnext');
+var nano         = require('gulp-cssnano');
+var shell        = require('gulp-shell');
+var autoprefixer = require('gulp-autoprefixer');
+var atImport     = require('postcss-import');
+var cp           = require('child_process');
+var browserSync  = require('browser-sync').create();
+var htmlmin      = require('gulp-html-minifier');
+
+var processors = [
+  atImport,
+  cssnext({
+    'browsers': ['last 2 version'],
+    'features': {
+      'customProperties': {
+        preserve: true,
+        appendVariables: true
+      },
+      'colorFunction': true,
+      'customSelectors': true,
+      'sourcemap': true,
+      'rem': false
+    }
+  })
+];
+
+// Default gulp.task
+// gulp.task('css', function() {
+//   gulp.src('./src/jkl-tachyons.css')
+//     .pipe(basswork())
+//     .pipe(size({gzip: false, showFiles: true, title:'basswork css'}))
+//     .pipe(size({gzip: true, showFiles: true, title:'basswork gzipped css'}))
+//     .pipe(gulp.dest('./css'))
+//     .pipe(minifyCSS())
+//     .pipe(rename({ extname: '.min.css' }))
+//     .pipe(size({gzip: false, showFiles: true, title:'basswork minified'}))
+//     .pipe(size({gzip: true, showFiles: true, title:'basswork minified'}))
+//     .pipe(gulp.dest('./css'));
+// });
+
+gulp.task('styles', function() {
+  return gulp.src(['./_assets/src/style.css'])
+  .pipe(postcss(processors))
+  .pipe(nano({discardComments: {removeAll: true}}))
+  .pipe(gulp.dest('./_assets/css'));
 });
 
-// Initialize browser-sync which starts a static server also allows for
-// browsers to reload on filesave
-gulp.task('browser-sync', function() {
-    browserSync.init(null, {
-        server: {
-            baseDir: "./_site/"
-        }
-    });
+gulp.task('buildSite', shell.task('bundle exec jekyll build --incremental'));
+
+gulp.task('minify', ['buildSite'], function() {
+  return gulp.src('_site/**/*.html')
+    .pipe(htmlmin({
+      collapseWhiteSpace: true,
+      removeTagWhitespace: true,
+      lint: true,
+      minifyJS: true
+      }))
+    .pipe(gulp.dest('_site'));
 });
 
-// Function to call for reloading browsers
-gulp.task('bs-reload', function () {
-    browserSync.reload();
+gulp.task('jekyll-build', shell.task(['bundle exec jekyll build --incremental --watch']));
+gulp.task('jekyll-build-once', ['buildSite', 'minify']);
+
+
+gulp.task('jekyll-serve', function() {
+  browserSync.init({ server: { baseDir: '_site/' } });
+  gulp.watch('./_assets/src/*.css', ['styles']);
+  gulp.watch('_site/**/*.*').on('change', browserSync.reload);
 });
 
-/*
-   DEFAULT TASK
+gulp.task('default', ['jekyll-build', 'jekyll-serve', 'styles']);
+gulp.task('build', ['styles', 'jekyll-build-once']);
 
- • Process sass then auto-prefixes and lints outputted css
- • Starts a server on port 3000
- • Reloads browsers when you change html or sass files
 
-*/
-gulp.task('default', ['css', 'bs-reload', 'browser-sync'], function(){
-  gulp.start(['css', 'bs-reload']);
-  gulp.watch('src/*', ['css']);
-  gulp.watch(['*.html', './**/*.html'], ['bs-reload']);
-});
-
+// Default setup
+// gulp.task('browser-sync', function() {
+//     browserSync.init(null, {
+//         server: {
+//             baseDir: "./_site/"
+//         }
+//     });
+// });
+//
+// gulp.task('bs-reload', function () {
+//     browserSync.reload();
+// });
+//
+// gulp.task('default', ['css', 'bs-reload', 'browser-sync'], function(){
+//   gulp.start(['css', 'bs-reload']);
+//   gulp.watch('src/*', ['css']);
+//   gulp.watch(['*.html', './**/*.html'], ['bs-reload']);
+// });
