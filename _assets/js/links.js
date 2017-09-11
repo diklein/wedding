558264a618839f97c08a0880e2099bd3be467407
David Klein <me@diklein.com>
2017-09-10 20:54:45 -0700

    Updating with progress

--- _assets/js/links.js
+++ _assets/js/links.js
@@ -1,23 +1,21 @@
-<script>
-   var eventsLink = document.querySelector("#events-link");
-   var accomodationsLink = document.querySelector("#accomodations-link");
-   var thingsLink = document.querySelector("#things-link");
-   var registryLink = document.querySelector("#registry-link");
-   var faqLink = document.querySelector("#faq-link");
-   var title = "{{ page.title }}";
-   var tag = "{{ page.tag }}";
-   if (title == "Events" || tag == "events") {
-      eventsLink.className = "blue no-underline mr3";
-   } else if (title == "Accomodations" || tag == "accomodations") {
-      accomodationsLink.className = "blue no-underline mr3";
-   }
-   else if (title == "Things to Do" || tag == "thingstodo") {
-      thingsLink.className = "blue no-underline mr3";
-   }
-   else if (title == "Registry" || tag == "registry") {
-      registryLink.className = "blue no-underline mr3";
-   }
-   else if (title == "FAQ" || tag == "faq") {
-      faqLink.className = "blue no-underline";
-   }
-</script>
+ var eventsLink = document.querySelector("#events-link");
+ var accomodationsLink = document.querySelector("#accomodations-link");
+ var thingsLink = document.querySelector("#things-link");
+ var registryLink = document.querySelector("#registry-link");
+ var faqLink = document.querySelector("#faq-link");
+ var title = "{{ page.title }}";
+ var tag = "{{ page.tag }}";
+ if (title == "Events" || tag == "events") {
+    eventsLink.className = "blue no-underline mr3";
+ } else if (title == "Accomodations" || tag == "accomodations") {
+    accomodationsLink.className = "blue no-underline mr3";
+ }
+ else if (title == "Things to Do" || tag == "thingstodo") {
+    thingsLink.className = "blue no-underline mr3";
+ }
+ else if (title == "Registry" || tag == "registry") {
+    registryLink.className = "blue no-underline mr3";
+ }
+ else if (title == "FAQ" || tag == "faq") {
+    faqLink.className = "blue no-underline";
+ }
